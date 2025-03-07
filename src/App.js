70804736508f7cd7eec5693e6454d558c1ff40bf
JohnB <johnbellamydev@gmail.com>
2025-03-06 18:44:05 -0600

    Rest of the code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Budget-App"> 
+        <h1 className="text-center text-green-300" >My Budget App</h1>
+        <p className="text-center">My Budget App is a simple web application that allows users to manage their budget.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
