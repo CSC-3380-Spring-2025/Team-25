f96d29635677c3a2d34e53fdfdbfc4b668f1c6da
Kennadi718 <168859814+Kennadi718@users.noreply.github.com>
2025-03-15 15:32:48 -0500

    Update Header.jsx

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import { Button } from '@/components/ui/button';
 
 
-function Header() {
+function Header({ logoSrc, buttonText}){
     return (
         <div className='p-5 flex justify-between items-center shadow-md'>
-            <Image src={'./logo.svg'}
+            <Image 
+            src={'./logo.svg'}
             alt='logo'
             width={50}
             height={30}
