0e30c053b024bd8ad18b16a66c0b2336cc4462b4
Phohou <danliu031505@gmail.com>
2025-03-29 14:39:54 -0500

    Added functionality to the {Get Started} button re routing users to the log in page

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Image from "next/image";
 import { Button } from '@/components/ui/button';
+import Login from './Login.jsx';
+import { useNavigate } from 'react-router-dom';
 
+function Header() {
 
-function Header({ logoSrc, buttonText}){
+    const handleclick = () => {
+        navigation.navigate('/Login.jsx')
+      };
+
     return (
-   LandingPage
         <div className='p-5 flex justify-between items-center border shadow-md'>
             <Image src={'./logo.svg'}
             alt='logo'
             width={50}
             height={30}
             />
-            <Button>Get Started</Button>
+            <Button onClick={handleclick}>Get Started</Button>
         </div>
     )
 }
