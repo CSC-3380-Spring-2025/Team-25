149a012941ef89bf92ffb4c44fb13a7b539faa02
Phohou <danliu031505@gmail.com>
2025-04-12 20:54:32 -0500

    Added API and dependacnies for clerk in order to run log in

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Image from "next/image";
 import { Button } from '@/components/ui/button';
 
 
-function Header({ logoSrc, buttonText}){
-    return (
-        <div className='p-5 flex justify-between items-center border shadow-md'>
-            <Image src={'./logo.svg'}
-            alt='logo'
-            width={50}
-            height={30}
-            />
-            <Button>Get Started</Button>
-        </div>
-    )
+function Header() {
+  const handleClick = () => {
+    console.log('Navigate to Login');
+  };
+
+  return (
+    <div className='p-5 flex items-center justify-between border-1 shadow-md'>
+      <Image 
+        src={'/logo.svg'}
+        alt='logo'
+        width={50}
+        height={30}
+      />
+      <div className='flex items-center gap-4'>
+        <Button> </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
