41327a354bbcfcd5f28360a48a5d077beab9dd5f
JohnB <johnbellamydev@gmail.com>
2025-04-25 19:02:49 -0500

    Completely Finished Database

--- src/utils/dbconfig.jsx
+++ src/utils/dbconfig.jsx
@@ -1,4 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-const sql = neon(process.env.DATABASE_URL);
+import * as schema from './schema';
+const sql = neon('postgresql://neondb_owner:npg_e34PyTNtXMSs@ep-sweet-shadow-a40pjvg7-pooler.us-east-1.aws.neon.tech/ExpensesDatabase?sslmode=require');//replace with process.env.DATABASE_URL
 const db = drizzle({ client: sql });
