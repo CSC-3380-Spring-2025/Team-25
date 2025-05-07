d1d162f9fc24d4ed5e469290138625f6ae150272
Phohou <danliu031505@gmail.com>
2025-05-07 18:09:03 -0500

    changed page.js to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg'; // if unused, you may remove this
 import './App.css';
 import Header from "./_components/Header";
 import Banner from "./_components/Banner";
 import Footer from "./_components/Footer";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <Header/>
-      <Banner/>
-      <Footer/>
+      <Header />
+      <Banner />
+      <Footer />
     </div>
   ); //this comment
-}
+};
 
 export default App;
